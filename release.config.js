f03fb42970c8957c42837a3e4f56e3f87d4b084c
Avril Li <linxforever@vip.qq.com>
2020-08-25 15:10:34 +0800

    fix: simplify semantic release config

--- release.config.js
+++ release.config.js
@@ -1,11 +1,15 @@
 module.exports = {
   verifyConditions: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     '@semantic-release/npm',
     '@semantic-release/git',
     '@semantic-release/github'
+    // '@semantic-release/commit-analyzer',
+    // '@semantic-release/release-notes-generator',
+    // '@semantic-release/changelog',
+    // '@semantic-release/npm',
+    // '@semantic-release/git',
+    // '@semantic-release/github'
   ],
   plugins: [
     [
